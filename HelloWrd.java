af5060d000c154be220968bc65a2cf1c496c61c0
Fonkeon Bule <65634341+Flashes2458@users.noreply.github.com>
2022-08-17 15:28:34 +0800

    Create HelloWrd.java

---
+++ HelloWrd.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+public class HelloWrd {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        int today = date.getDayOfMonth();
+        LocalDate firstDayOfMonth = date.minusDays(today-1);
+        int firstDayOfWeekValue= firstDayOfMonth.getDayOfWeek().getValue();
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for(int i=1;i<firstDayOfWeekValue;++i){
+            System.out.print("    ");
+        }
+        int lastDayOfMonthValue = firstDayOfMonth.plusMonths(1).minusDays(1).getDayOfMonth();
+        int k = firstDayOfWeekValue;
+        for(int j=1;j<=lastDayOfMonthValue;++j,++k){
+            System.out.printf("%3d",j);
+            if(j==today)
+                System.out.print("*");
+            else
+                System.out.print(" ");
+            if(k%7==0)
+                System.out.println();
+        }
+    }
+}
